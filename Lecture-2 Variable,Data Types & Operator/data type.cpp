88ef653661d993c07bc8779f807f72da82c85414
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-06-07 17:37:21 +0600

    added data type

---
+++ Lecture-2 Variable,Data Types & Operator/data type.cpp
@@ -0,0 +1,9 @@
+#include<iostream> 
+using namespace std;
+int main() {
+   int  age = 25;
+   //cout << "age"; // ekhane "age" dile age just print korche 
+   cout << age << endl;  //ekhane age double quotation othanote age er value print korche jeta 25 store kora hoyeche 
+    return 0;
+    
+}
